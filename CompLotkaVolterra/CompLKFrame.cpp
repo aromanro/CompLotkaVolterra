f51dc05385bb2de2834197d78e7710bb97bcddf4
Adrian Roman <aroman_ro@yahoo.com>
2018-08-04 17:13:59 +0300

    Minor changes

--- CompLotkaVolterra/CompLKFrame.cpp
+++ CompLotkaVolterra/CompLKFrame.cpp
@@ -296,7 +296,7 @@ void CompLKFrame::OnTimer(wxTimerEvent& WXUNUSED(event))
 		vtkVector2d screenPos(mouseEvent.GetScreenPos().Cast<double>().GetData());
 		vtkVector2d lastScreenPos(mouseEvent.GetLastScreenPos().Cast<double>().GetData());
 		pChart->MouseMoveEvent(mouseEvent);
-
+		
 		pChart->Update();
 
 		Refresh();
