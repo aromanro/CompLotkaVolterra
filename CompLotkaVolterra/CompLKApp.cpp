369db1cd34f57ae737e18bca282f4021b3aa5768
Adrian Roman <aroman_ro@yahoo.com>
2018-09-05 20:14:51 +0300

    Call OnInit from application base class

--- CompLotkaVolterra/CompLKApp.cpp
+++ CompLotkaVolterra/CompLKApp.cpp
@@ -7,6 +7,9 @@ wxIMPLEMENT_APP(CompLKApp);
 
 bool CompLKApp::OnInit()
 {
+	if (!wxApp::OnInit())
+		return false;
+
 	frame = new CompLKFrame("CompLotkaVolterra", wxPoint(50, 50), wxSize(1024, 800));
 	frame->Show(true);
 
